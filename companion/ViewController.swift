faa5b95971ac422cc7a206c58879c7fde48a51d6
yves songolo <yves.songolo@gmail.com>
2018-09-06 11:00:48 -0700

    initial settings

---
+++ companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  companion
+//
+//  Created by Yves Songolo on 9/6/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
